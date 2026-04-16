b074e95d3d9d956aad9ec63cff34e8f19148a010
sonuprajapati9794 <prajapati979473@gmail.com>
2026-04-16 15:52:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function findDomLevel() {
+  // Get the target element
+  let element = document.getElementById("level");
+
+  let level = 0;
+
+  // Traverse up the DOM tree
+  while (element) {
+    level++;
+    element = element.parentElement;
+  }
+
+  // Show result
+  alert("The level of the element is: " + level);
+}
+
+// Call function (ensure DOM is loaded)
+window.onload = findDomLevel;
